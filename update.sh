9f43c25d73c04f13359bab5592a55421cb4a7554
Patrick Meenan <PatMeenan@gmail.com>
2021-02-01 15:09:54 -0500

    Tweaked the update script to set the remote upstream

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 git checkout master
 git pull origin master
+git remote rm upstream || true
+git remote add upstream https://github.com/AliasIO/wappalyzer.git
 git fetch upstream
 git checkout master
 git merge upstream/master
 git push
+git remote rm upstream
